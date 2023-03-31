fa927598111d158322c6d1efe94c539015a8c5bc
julianmedwards <julian.michael.edwards@gmail.com>
2023-03-31 13:09:05 -0500

    Working search for pets and owners.

--- app/public/js/search.js
+++ app/public/js/search.js
@@ -4,17 +4,26 @@ document.onload = initSearch()
 
 function initSearch() {
     document.getElementById('search').addEventListener('submit', search)
+
+    document
+        .getElementById('search-results')
+        .addEventListener('hidden.bs.offcanvas', (event) => {
+            event.currentTarget.querySelector('.offcanvas-body').innerHTML = ''
+        })
 }
 
 async function search(event) {
     event.preventDefault()
-    console.log('Searching')
 
     const results = await searchRequest(
         document.getElementById('search').querySelector('input').value
     )
 
-    console.log(results)
+    const resultsMenu = document.getElementById('search-results')
+    buildSearchResults(resultsMenu.querySelector('.offcanvas-body'), results)
+
+    const bsOffCanvas = bootstrap.Offcanvas.getOrCreateInstance(resultsMenu)
+    bsOffCanvas.show()
 }
 
 async function searchRequest(queryString) {
@@ -32,4 +41,52 @@ async function searchRequest(queryString) {
     } else {
         console.error('Error fetching search results.')
     }
+}
+
+function buildSearchResults(wrapper, results) {
+    if (results.owners.length === 0 && results.pets.length === 0) {
+        wrapper.append(
+            (document.createElement('h5').textContent = 'No results found.')
+        )
+    }
+
+    if (results.owners.length > 0) {
+        const header = document.createElement('h5')
+        header.textContent = 'Owners'
+        wrapper.append(header)
+        wrapper.append(buildResultsGroup(results.owners, 'owners'))
+    }
+
+    if (results.pets.length > 0) {
+        const header = document.createElement('h5')
+        header.textContent = 'Pets'
+        wrapper.append(header)
+        wrapper.append(buildResultsGroup(results.pets, 'pets'))
+    }
+}
+
+function buildResultsGroup(data, groupName) {
+    const wrapper = document.createElement('ul')
+    wrapper.classList.add('list-group', 'list-group-flush')
+    data.forEach((result) => {
+        const el = buildResult(result, groupName)
+        el.classList.add(groupName)
+        wrapper.append(el)
+    })
+
+    return wrapper
+}
+
+function buildResult(data, groupName) {
+    const resultEl = document.createElement('a')
+    resultEl.classList.add(
+        'list-group-item',
+        'list-group-item-action',
+        'link-primary'
+    )
+    resultEl.textContent = data.name
+
+    resultEl.setAttribute('href', `/${groupName}/${data.id}`)
+
+    return resultEl
 }
